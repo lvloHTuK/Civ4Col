bc9a03fc361613ff86c5b01f47825d52410b626f
XetPy1030 <xetpy1030@mail.ru>
2024-02-15 17:15:22 +0300

    Auto get changes

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
-@REM git pull and start Colonization.exe from current directory
+@REM git pull and start Colonization.exe from current directory; if error, inform user and input
 
-git reset --hard && git pull && start Colonization.exe
+git reset --hard && git pull && start Colonization.exe || echo "Error: could not start game. Please check the log for details." && pause
