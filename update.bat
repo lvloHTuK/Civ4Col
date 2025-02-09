f14631b651d0375283598d4dd448ef0cab45a243
lvloHTuK <sibmag2645@gmail.com>
2025-02-09 15:57:11 +0300

    Исправление бага с едой

--- update.bat
+++ update.bat
@@ -3,7 +3,7 @@ setlocal
 set /a "count=0"
 
 :retry
-git reset --hard && git pull && start Colonization.exe
+git reset --hard && git pull && start C:\Games\Civ4ColKLT\Colonization.exe
 
 if %errorlevel% neq 0 (
     echo Error: could not start game. Please check the log for details.
